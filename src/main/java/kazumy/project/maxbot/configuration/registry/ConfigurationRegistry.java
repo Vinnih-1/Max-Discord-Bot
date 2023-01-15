ad4a816926a7a9a86dcde0f590a23acd03c13900
Vinnih-1 <vinifantal@hotmail.com>
2023-01-15 15:01:55 -0300

    :wrench: registry new configuration values

--- src/main/java/kazumy/project/maxbot/configuration/registry/ConfigurationRegistry.java
+++ src/main/java/kazumy/project/maxbot/configuration/registry/ConfigurationRegistry.java
@@ -1,14 +1,11 @@
 package kazumy.project.maxbot.configuration.registry;
 
 import com.henryfabio.minecraft.configinjector.bukkit.injector.BukkitConfigurationInjector;
+import kazumy.project.maxbot.configuration.basic.CommandValue;
 import kazumy.project.maxbot.configuration.basic.DiscordValue;
 import kazumy.project.maxbot.configuration.basic.PaymentValue;
-import kazumy.project.maxbot.configuration.embed.CloseTicketEmbedValue;
-import kazumy.project.maxbot.configuration.embed.ServiceEmbedValue;
-import kazumy.project.maxbot.configuration.embed.TermsEmbedValue;
-import kazumy.project.maxbot.configuration.embed.TicketEmbedValue;
-import kazumy.project.maxbot.configuration.menu.ServiceMenuValue;
-import kazumy.project.maxbot.configuration.menu.TicketMenuValue;
+import kazumy.project.maxbot.configuration.embed.*;
+import kazumy.project.maxbot.configuration.menu.*;
 import lombok.Data;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -22,12 +19,20 @@ public class ConfigurationRegistry {
                 .injectConfiguration(
                         PaymentValue.instance(),
                         DiscordValue.instance(),
+                        CommandValue.instance(),
 
                         TicketEmbedValue.instance(),
                         ServiceEmbedValue.instance(),
                         TermsEmbedValue.instance(),
                         CloseTicketEmbedValue.instance(),
+                        CashEmbedValue.instance(),
+                        VipEmbedValue.instance(),
+                        UnbanEmbedValue.instance(),
+                        NicknameEmbedValue.instance(),
 
+                        CashMenuValue.instance(),
+                        UnbanMenuValue.instance(),
+                        VipMenuValue.instance(),
                         TicketMenuValue.instance(),
                         ServiceMenuValue.instance()
                 );
