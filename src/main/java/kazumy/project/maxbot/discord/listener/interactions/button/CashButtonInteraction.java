75e2cacb9fed2e1ce6e5b6864a5fd46d5a03e987
Vinnih-1 <vinifantal@hotmail.com>
2023-01-16 03:05:29 -0300

    :white_check_mark: new button interaction to get player nickname via modal

---
+++ src/main/java/kazumy/project/maxbot/discord/listener/interactions/button/CashButtonInteraction.java
@@ -0,0 +1,24 @@
+package kazumy.project.maxbot.discord.listener.interactions.button;
+
+import kazumy.project.maxbot.discord.listener.InteractionService;
+import lombok.val;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
+import net.dv8tion.jda.api.interactions.components.Modal;
+import net.dv8tion.jda.api.interactions.components.text.TextInput;
+import net.dv8tion.jda.api.interactions.components.text.TextInputStyle;
+
+public class NicknameButtonInteraction extends InteractionService<ButtonInteractionEvent> {
+
+    public NicknameButtonInteraction() {
+        super("nickname");
+    }
+
+    @Override
+    public void execute(ButtonInteractionEvent event) {
+        val modal = Modal.create("modalname", "Insira o seu Nickname");
+        modal.addActionRow(TextInput.create("name", "Name", TextInputStyle.SHORT)
+                .setPlaceholder("Nickname")
+                .build());
+        event.replyModal(modal.build()).queue();
+    }
+}
