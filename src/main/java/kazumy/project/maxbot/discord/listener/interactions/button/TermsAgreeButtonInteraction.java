0801ec030651ee9ed3368d85ebf24981c7e8dc55
Vinnih-1 <vinifantal@hotmail.com>
2023-01-16 03:12:32 -0300

    :bug: send the close ticket embed with close ticket button

--- src/main/java/kazumy/project/maxbot/discord/listener/interactions/button/TermsAgreeButtonInteraction.java
+++ src/main/java/kazumy/project/maxbot/discord/listener/interactions/button/TermsAgreeButtonInteraction.java
@@ -5,6 +5,7 @@ import kazumy.project.maxbot.configuration.embed.ServiceEmbedValue;
 import kazumy.project.maxbot.configuration.menu.ServiceMenuValue;
 import kazumy.project.maxbot.discord.listener.InteractionService;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
 
 public class TermsAgreeButtonInteraction extends InteractionService<ButtonInteractionEvent> {
 
@@ -15,9 +16,11 @@ public class TermsAgreeButtonInteraction extends InteractionService<ButtonIntera
     @Override
     public void execute(ButtonInteractionEvent event) {
         event.getMessage().delete().queue();
-        event.getChannel().sendMessageEmbeds(
-                        CloseTicketEmbedValue.instance().toEmbed(),
-                        ServiceEmbedValue.instance().toEmbed())
+        event.getChannel().sendMessageEmbeds(CloseTicketEmbedValue.instance().toEmbed())
+                .addActionRow(Button.danger("close-ticket-button", "Fechar ticket"))
+                .queue();
+
+        event.getChannel().sendMessageEmbeds(ServiceEmbedValue.instance().toEmbed())
                 .addActionRow(ServiceMenuValue.instance().toMenu("services"))
                 .queue();
     }
